c46125e403e23ea1d398b3a23af6ba1772561d39
mahendra761 <mahendragawande90@gmail.com>
2023-04-25 15:44:32 +0530

    first commit

---
+++ DRC Group/DRC-Group-main/js/readmore.js
@@ -0,0 +1,135 @@
+// Omaxe city-1
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<b>Read more</b>";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<b>Read less</b>";
+      moreText.style.display = "inline";
+    }
+}
+
+// // Omaxe city-2
+function myFunction1() {
+    var dots1 = document.getElementById("dots1");
+    var moreText1 = document.getElementById("more1");
+    var btnText1 = document.getElementById("myBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = "<b>Read more</b>";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "<b>Read less</b>";
+      moreText1.style.display = "inline";
+    }
+}
+// Omaxecity- hills
+function myFunction2() {
+    var dots2 = document.getElementById("dots2");
+    var moreText2= document.getElementById("more2");
+    var btnText2= document.getElementById("myBtn2");
+  
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btnText2.innerHTML = "<b>Read more</b>";
+      moreText2.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btnText2.innerHTML = "<b>Read less</b>";
+      moreText2.style.display = "inline";
+    }
+}
+//  Omaxe City-Ratlam-->
+function myFunction3() {
+    var dots3= document.getElementById("dots3");
+    var moreText3= document.getElementById("more3");
+    var btnText3= document.getElementById("myBtn3");
+  
+    if (dots3.style.display === "none") {
+      dots3.style.display = "inline";
+      btnText3.innerHTML = "<b>Read more</b>";
+      moreText3.style.display = "none";
+    } else {
+      dots3.style.display = "none";
+      btnText3.innerHTML = "<b>Read less</b>";
+      moreText3.style.display = "inline";
+    }
+}
+
+  
+//  Vistara city -1-->
+function myFunction4() {
+    var dots4= document.getElementById("dots4");
+    var moreText4= document.getElementById("more4");
+    var btnText4= document.getElementById("myBtn4");
+  
+    if (dots4.style.display === "none") {
+      dots4.style.display = "inline";
+      btnText4.innerHTML = "<b>Read more</b>";
+      moreText4.style.display = "none";
+    } else {
+      dots4.style.display = "none";
+      btnText4.innerHTML = "<b>Read less</b>";
+      moreText4.style.display = "inline";
+    }
+}
+
+//  Vistara city-2-->
+function myFunction5() {
+    var dots5= document.getElementById("dots5");
+    var moreText5= document.getElementById("more5");
+    var btnText5= document.getElementById("myBtn5");
+  
+    if (dots5.style.display === "none") {
+      dots5.style.display = "inline";
+      btnText5.innerHTML = "<b>Read more</b>";
+      moreText5.style.display = "none";
+    } else {
+      dots5.style.display = "none";
+      btnText5.innerHTML = "<b>Read less</b>";
+      moreText5.style.display = "inline";
+    }
+}
+
+//  Vistara Hills-->
+function myFunction6() {
+    var dots6= document.getElementById("dots6");
+    var moreText6= document.getElementById("more6");
+    var btnText6= document.getElementById("myBtn6");
+  
+    if (dots6.style.display === "none") {
+      dots6.style.display = "inline";
+      btnText6.innerHTML = "<b>Read more</b>";
+      moreText6.style.display = "none";
+    } else {
+      dots6.style.display = "none";
+      btnText6.innerHTML = "<b>Read less</b>";
+      moreText6.style.display = "inline";
+    }
+}
+
+
+  //  Omaxe City-Ratlam
+function myFunction7() {
+    var dots7= document.getElementById("dots7");
+    var moreText7= document.getElementById("more7");
+    var btnText7= document.getElementById("myBtn7");
+  
+    if (dots7.style.display === "none") {
+      dots7.style.display = "inline";
+      btnText7.innerHTML = "<b>Read more</b>";
+      moreText7.style.display = "none";
+    } else {
+      dots7.style.display = "none";
+      btnText7.innerHTML = "<b>Read less</b>";
+      moreText7.style.display = "inline";
+    }
+}
